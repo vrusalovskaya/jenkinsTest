54f9ab01ae24a0779805a9cf35c669aeb7ccedda
vrusalovskaya <143196375+vrusalovskaya@users.noreply.github.com>
2024-09-08 19:09:37 +0400

    Update TomcatController.java

--- src/main/java/com/example/jenkinstest/TomcatController.java
+++ src/main/java/com/example/jenkinstest/TomcatController.java
@@ -12,7 +12,7 @@ public class TomcatController {
     @GetMapping("/hello")
     public Collection<String> sayHello() {
         return IntStream.range(0, 10)
-                .mapToObj(i -> "Hello number try10" + i)
+                .mapToObj(i -> "Hello number try11" + i)
                 .collect(Collectors.toList());
     }
 }
